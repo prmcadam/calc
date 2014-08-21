5203f766287c55f9ec320860885e7af1a4b5341d
Paul McAdam <prmcadam@gmail.com>
2014-08-21 14:45:47 +1000

    Add multiply function

--- calc.py
+++ calc.py
@@ -1,11 +1,15 @@
 import sys
 
-def add_all()
+def add_all(nums):
+	return sum(nums)
 
+def multiply_all(nums):
+	return reduce(lambda a, b: a*b, nums)
 
 if __name__ == '__main__':
 	command =sys.argv[1]
 	nums=sys.argv[2:]
 	if command=='add':
 		print add_all(nums)
-
+	if command=='multiply':
+		print multiply_all(nums)
